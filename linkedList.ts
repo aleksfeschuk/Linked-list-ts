77c254786288da8ba95a58f8195d7a536f188433
Aleksandr Feshchuk <aleksfeschuk@gmail.com>
2025-04-04 00:03:58 -0600

    here's what the list looks like and what methods it has

---
+++ linkedList.ts
@@ -0,0 +1,18 @@
+interface Node {
+    value: string | null;
+    nextNode: Node | null;
+}
+
+interface LinkedList {
+    head: Node | null;
+    append(value: string): void;
+    prepend(value: string): void;
+    size(): number;
+    getHead(): Node | null;
+    tail(): Node | null;
+    at(index: number): Node | null;
+    pop(): void;
+    contains(value: string): boolean;
+    find(value: string): number | null;
+    toString(): string;
+}
